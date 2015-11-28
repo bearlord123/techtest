fc7f6ace475fbfeb0f64097dae90b6f942499cad
bearlord123 <alex891220@gmail.com>
2015-11-28 04:11:40 -0500

    second edition

--- js/fetch.js
+++ js/fetch.js
@@ -8,10 +8,16 @@ $.getJSON	("http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20we
 		})
 	$("<ul id='day'></ul>").appendTo("#nav");
 	$(items.join('')).appendTo("#day");
-	$("#main").html(now.title.substring(14,25));
+	var reg=/for\s(.+),\s([A-Z]{2})\s/g;
+	var text = now.title;
+
+	var matched = reg.exec(text);
+	var city = matched[1];
+	var state = matched[2];
+	var image = $.parseHTML(now.description);
+	$("#main").html(city+', '+state);
 	$('<div class="now">'+now.condition.temp+"&deg"+'</div>').appendTo("#main");
 	$('<div class="spec"><img src='+now.description.substring(11,48)+'>'+'<br/>'+'<p>'+now.condition.text+'</p></div>').appendTo(".now");
-	
 	
 });
 })(jQuery);
